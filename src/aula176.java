529f743d3cbb1d07a7cf91c67481605d74e9d8c8
Alexandro Silva <alexandrorsilva2@gmail.com>
2020-02-29 22:17:15 -0300

    Aula 176 - Aula completan

--- src/aula176.java
+++ src/aula176.java
@@ -1,10 +1,41 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class aula176 {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Aula 176");
-
+		String path = "C:\\Users\\Alexandro\\Documents\\TREINAMENTO JAVA COMPLETO - UDEMY\\in.dtxt";
+		
+		FileReader fr = null;
+		BufferedReader br = null;
+		
+		try {
+			fr = new FileReader(path);
+			br = new BufferedReader(fr);
+			
+			String line = br.readLine();
+			
+			while (line != null) {			
+				System.out.println(line);
+				line = br.readLine();
+			}
+			
+		} catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		} finally {
+			try {
+				if (br != null) {
+					br.close();
+				}
+				if (fr != null) {
+					fr.close();
+				}
+				
+			} catch (IOException e) {
+				e.printStackTrace();
+			}			
+		}
 	}
-
 }

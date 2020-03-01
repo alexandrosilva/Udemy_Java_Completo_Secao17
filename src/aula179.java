3320835b3ac2c73655ddf3592b9e8106fd32f0ad
Alexandro Silva <alexandrorsilva2@gmail.com>
2020-02-29 23:04:28 -0300

    Aula 179 - Aula completa

--- src/aula179.java
+++ src/aula179.java
@@ -13,11 +13,13 @@ public class aula179 {
 		File path = new File(strPath);		
 		File[] folders = path.listFiles(File::isDirectory);
 
+		// Verificar as subpastas da pasta
 		System.out.println("FOLDERS:");
 		for (File folder : folders) {
 			System.out.println(folder);
 		}
 		
+		// Verificar os arquivos da pasta
 		File[] files = path.listFiles(File::isFile);
 		
 		System.out.println("ARQUIVOS:");
@@ -25,6 +27,15 @@ public class aula179 {
 			System.out.println(file);
 		}
 	
+		// Criar subpasta	
+		boolean sucess = new File(strPath + "\\teste").mkdir();
+		System.out.println("Arquivo criado com sucesso??? " + sucess);
+		
+		folders = path.listFiles(File::isDirectory);
+		System.out.println("FOLDERS:");
+		for (File folder : folders) {
+			System.out.println(folder);
+		}
 		sc.close();
 	}
 }

402e68f91cef225dcb0da282a524af139ef54e8a
Alexandro Silva <alexandrorsilva2@gmail.com>
2020-02-29 22:40:56 -0300

    Aula 178 - Aula completa

--- src/aula178.java
+++ src/aula178.java
@@ -5,11 +5,10 @@ import java.io.IOException;
 public class aula178 {
 
 	public static void main(String[] args) {
-
 		String[] lines = new String[] {"Ola", "beleza", "Tudo certo"};
 		
 		String path = "C:\\Users\\Alexandro\\Documents\\TREINAMENTO JAVA COMPLETO - UDEMY\\out.txt";
-
+		
 		try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))){
 			for (String line : lines) {
 				bw.write(line);
